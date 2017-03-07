7e78f6a5f2292b11e9a8ddbbd673d3a282d06ce5
Oswald <oswald.capriles46@gmail.com>
2017-03-07 17:40:45 -0400

    Added remove parity bits and correction

--- link/binary.js
+++ link/binary.js
@@ -76,31 +76,99 @@ function processChunk(chunk) {
 
 /* Add an argument with hamming number? */
 
+
+const parityCount = (bCount) => Math.ceil(Math.log2(bCount+1));
+
+exports.remParityBits = (bits) => {
+
+	const bitArr = bits.split('');
+	const pCount = parityCount(bits);
+
+	for (let p=(1 << pCount); p > 0; p>>=1) 
+		bitArr.splice(p-1, 1);
+
+	return bitArr.join('');
+}
+
+/*
+function setParityBits(bits, pCount) {
+
+	const bCount = bits.length;
+	const buf   = new ArrayBuffer(pCount);
+	const pBits = new Uint8Array(buf).fill(0);
+
+	const parity = ()
+
+	for (let i=1; i <= bCount; i++) {
+		if ((i & i-1) != 0) {
+			for (let j=0; j < pCount; j++) {
+				if (((i >> j) & 1) && bits[i-1] == 1)
+					pBits[j] ^= 1;
+			}
+		}
+	}
+}
+*/
+
 exports.hamming = (bits) => {
 
 	let bitL = bits.split('');
-	let p = 1;
 	let res = bits.length;
+	let i = 0;
+	let p = 1;
 
-	while(res > 0) {
+	for (i=0, p=1; res > 0; i+=1, p<<=1) {
 		bitL.splice(p-1, 0, 0); /* Important! */
 		res -= (p << 1) - p - 1;
-		p <<= 1;
 	}
 
-	const np = Math.log2(p);
-
+	const pCount = i;
 	for (let i=1; i <= bitL.length; i++) {
-		
 		if ((i & i-1) != 0) {
-			for (let j=0, p=1; j < np; j++, p<<=1) {
+			for (let j=0, p=1; j < pCount; j++, p<<=1) {
 				if (((i >> j) & 1) && bitL[i-1] === '1') 
 					bitL[p-1] ^= 1;
 			}
 		}
 	}
-	
+
 	return bitL.join('');
+}
+
+exports.hammingCorrect = (bits) => {
+
+	const bitArr = bits.split('');
+	const bCount = bits.length;
+	const pCount = Math.ceil(Math.log2(bCount+1));
+
+	const buf   = new ArrayBuffer(pCount);
+	const pBits = new Uint8Array(buf).fill(0);
+
+	for (let i=1; i <= bCount; i++) {
+		if((i & i-1) != 0) {
+			for (let j=0; j < pCount; j++) {
+				if (((i >> j) & 1) && bits[i-1] == 1)
+					pBits[j] ^= 1;
+			}
+		}
+	}
+
+	let err = 0;
+	for(let i=0, p=1; p < bCount; i++, p<<=1) {
+		if (pBits[i] != bits[p-1]) err += p;
+	}
+
+	const bad = err - 1;
+
+	/* La inmutabilidad del cangrejo */
+	if (bad >= 0 && bad < bCount) 
+		bitArr[bad] = bits[bad] == 1 ? 0 : 1;
+
+	/* Should not join */
+	return {
+		bits: bitArr.join(''),
+		err: bad
+	};
 }
 
 function readFile(path) {
