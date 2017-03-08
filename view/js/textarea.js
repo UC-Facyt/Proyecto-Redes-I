00e7a7d70953029bf0a6d59f1db0abc26eeae1e7
Oswald <oswald.capriles46@gmail.com>
2017-03-08 01:09:00 -0400

    Shitty textarea

--- view/js/textarea.js
+++ view/js/textarea.js
@@ -4,15 +4,21 @@
 
 	const chatMsg = $('.chat-messages');
 	const chatBox = $('.chat-textarea');
+	const text = $('textarea');
 	let extraPixels = 0;
 
 	$('#chat-box').on('keydown', (e) => {
-
-		if (extraPixels < 100) {
-			extraPixels += 15;
-			chatBox.css('height', `${45+extraPixels}px`);
-			chatMsg.css('height', `calc(100vh - (${155+extraPixels}px))`);
-		}
-
+		console.log(text);
+		console.log(chatBox.css('height'))
+		chatBox.css('height', `calc(45px + ${e.target.scrollHeight}px)`);
+		chatMsg.css('height', `calc(100vh - (${155+e.target.scrollHeight}px))`);
 	});
+
+	$('textarea').each(function () {
+		this.setAttribute('style', `height: calc(100% - ${this.scrollHeight+24}px);`);
+	}).on('input', function () {
+		this.style.height = 'auto';
+		this.style.height = (this.scrollHeight) + 'px';
+	});
+
 })();
