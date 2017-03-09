5d1a0d97f0547e4c1aad0f47eb7aaafb4699f6a9
Oswald <oswald.capriles46@gmail.com>
2017-03-08 20:51:27 -0400

    Exported process chunk

--- dll/app.js
+++ dll/app.js
@@ -1,125 +1,119 @@
-(function() {
+const config = require('./config.js');
+const fs = require('fs');
+const bin = require('./binary.js');
+const crc = require('./crc.js');
+const ham = require('./hamming.js');
+const flg = require('./flags.js');
 
-	const config = require('./config.js');
-	const fs = require('fs');
-	const bin = require('./binary.js');
-	const crc = require('./crc.js');
-	const ham = require('./hamming.js');
-	const flg = require('./flags.js');
+function createPacket(packet) {
+	console.log('Rosquete');
+	console.log(packet);
 
-	function createPacket(packet) {
-		console.log('Rosquete');
-		console.log(packet);
+	console.log('\nConteo de caracteres:')
+	packet = bin.addCharCont(packet);
+	console.log(packet);
 
-		console.log('\nConteo de caracteres:')
-		packet = bin.addCharCont(packet);
-		console.log(packet);
+	switch (config.MODE) {
+	 	case 'crc':
+	 		console.log('\nCRC-16:')
+	 		packet = crc.crc_16(packet);
+	 		console.log(packet);
+	 		break;
 
-		switch (config.MODE) {
-		 	case 'crc':
-		 		console.log('\nCRC-16:')
-		 		packet = crc.crc_16(packet);
-		 		console.log(packet);
-		 		break;
+	 	case 'hamming':
+	 		console.log('\nHamming:')
+	 		packet = ham.segHamming(packet);
+	 		console.log(packet);
+	 		break
 
-		 	case 'hamming':
-		 		console.log('\nHamming:')
-		 		packet = ham.segHamming(packet);
-		 		console.log(packet);
-		 		break
-
-		 	default:
-		 		break;
-		}
-
-		console.log('\nRelleno:')
-		packet = flg.relleno(packet);
-		console.log(packet);
-
-		console.log('\nBanderas:')
-		packet = flg.banderiador(packet);
-		console.log(packet);
-		return packet;
+	 	default:
+	 		break;
 	}
 
-	function disasPacket(packet) {
+	console.log('\nRelleno:')
+	packet = flg.relleno(packet);
+	console.log(packet);
 
-		console.log('\nBanderas:');
-		packet = flg.apatrida(packet);
-		console.log(packet);
+	console.log('\nBanderas:')
+	packet = flg.banderiador(packet);
+	console.log(packet);
+	return packet;
+}
 
-		console.log('\nRelleno:');
-		packet = flg.desRelleno(packet);
-		console.log(packet);
+function disasPacket(packet) {
 
-		switch (config.MODE) {
-			case 'crc':
-		 		console.log('\nCRC-16:');
-		 		console.log(crc.crc_16_Ver(packet) ? 'Nene' : 'Pupu');
-		 		packet = crc.unCrc(packet);
-		 		console.log(packet);
-		 		break;
+	console.log('\nBanderas:');
+	packet = flg.apatrida(packet);
+	console.log(packet);
 
-		 	case 'hamming':
-		 		console.log('\nHamming:');
+	console.log('\nRelleno:');
+	packet = flg.desRelleno(packet);
+	console.log(packet);
 
-		 		let code = ham.correctPacket(packet);
-		 		for (let s of code) {
-		 			console.log(s);
-		 		}
+	switch (config.MODE) {
+		case 'crc':
+	 		console.log('\nCRC-16:');
+	 		console.log(crc.crc_16_Ver(packet) ? 'Nene' : 'Pupu');
+	 		packet = crc.unCrc(packet);
+	 		console.log(packet);
+	 		break;
 
-		 		packet = code.map(h => {
-		 			if (h.err != -1) console.log('Ay papa: ', h.err);
-		 			return h.bits;
-		 		}).join('');
+	 	case 'hamming':
+	 		console.log('\nHamming:');
 
-		 		console.log(packet);
+	 		let code = ham.correctPacket(packet);
+	 		for (let s of code) {
+	 			console.log(s);
+	 		}
 
-		 		packet = ham.remPacketParityBits(packet);
-		 		console.log(packet);
+	 		packet = code.map(h => {
+	 			if (h.err != -1) console.log('Ay papa: ', h.err);
+	 			return h.bits;
+	 		}).join('');
 
-		 		break
+	 		console.log(packet);
 
-		 	default:
-		 		break;
-		}
+	 		packet = ham.remPacketParityBits(packet);
+	 		console.log(packet);
 
-		console.log('\nConteo:');
-		console.log(bin.bytesContCheck(packet) ? 'Nene' : 'Pupu');
-		packet = bin.remCharCont(packet);
+	 		break
 
-		console.log(packet);
-
-		return packet;
+	 	default:
+	 		break;
 	}
 
-	/* Sweg */
-	function processChunk(chunk) {
+	console.log('\nConteo:');
+	console.log(bin.bytesContCheck(packet) ? 'Nene' : 'Pupu');
+	packet = bin.remCharCont(packet);
 
-		for (let b of bin.bytes2Bits(chunk)) {
-			let trama = createPacket(b);
+	console.log(packet);
+
+	return packet;
+}
+
+exports.processChunk = (chunk) => {
+
+	for (let b of bin.bytes2Bits(chunk)) {
+		let trama = createPacket(b);
 
 
-			let joseEnrique = Math.floor(Math.random() * trama.length);
-			let thing = trama.split('');
-			thing[joseEnrique] = (thing[joseEnrique] == 1) ? 0 : 1;
+		let joseEnrique = Math.floor(Math.random() * trama.length);
+		let thing = trama.split('');
+		thing[joseEnrique] = (thing[joseEnrique] == 1) ? 0 : 1;
 
-			let packet = thing.join('');
-			/* Escribir en tramas.txt */
+		let packet = thing.join('');
+		/* Escribir en tramas.txt */
 
-			console.log(disasPacket(packet));
-		}
+		console.log(disasPacket(packet));
 	}
+}
 
+exports.readFile = (path) => {
+	const stream = fs.createReadStream(path);
+	stream.on('data' , processChunk);
+	stream.on('error', (err) => {
+		console.log(err);
+	});
+}
 
-	function readFile(path) {
-		const stream = fs.createReadStream(path);
-		stream.on('data' , processChunk);
-		stream.on('error', (err) => {
-			console.log(err);
-		});
-	}
-	
-	readFile(config.MSG_FILE);
-
-})();
+// readFile(config.MSG_FILE);
